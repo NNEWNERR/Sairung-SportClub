005a8006221ae3fb32bcb85921bffd6087107446
newindistinct <new.indistinct@gmail.com>
2024-12-11 08:50:09 +0700

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAl_mGwylLvWYjxHNjzaDWtNguZFT2cq8Y",
+    authDomain: "sairung-sportclub.firebaseapp.com",
+    projectId: "sairung-sportclub",
+    storageBucket: "sairung-sportclub.firebasestorage.app",
+    messagingSenderId: "87880733187",
+    appId: "1:87880733187:web:7c31dcadc2e81104a9b436",
+    measurementId: "G-296SEDXQWM"
+  }
+};
